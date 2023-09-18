9e166747bb16f62ca424bca96a4966d4494555b6
Otto <ottotonsorialist@gmail.com>
2023-09-17 21:22:03 -0400

    bumping version number correctly

--- utils/constants.py
+++ utils/constants.py
@@ -5,7 +5,7 @@ import sys
 
 class Constants:
     def __init__(self):
-        self.APP_VERSION = "v2.5g"
+        self.APP_VERSION = "v2.6a"
         self.APP_RELEASE_DATE = "2023-Sept-17"
 
         self.NUZLOCKE_APP_VERSION = "v0.5a"
